84805ad921f9a4263981a681a23ddfdbfa9bc500
Endi Jean <endijean91@gmail.com>
2022-05-11 22:11:14 -0300

    endpoint findAll para user

--- src/main/java/com/endijean/apitestes/resource/UserResource.java
+++ src/main/java/com/endijean/apitestes/resource/UserResource.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -21,5 +24,11 @@ public class UserResource {
     @GetMapping(value="/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok().body(service.findAll()
+                .stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
     }
 }
--- src/main/java/com/endijean/apitestes/services/UserService.java
+++ src/main/java/com/endijean/apitestes/services/UserService.java
@@ -2,7 +2,11 @@ package com.endijean.apitestes.services;
 
 import com.endijean.apitestes.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
