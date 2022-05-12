bba9e02e25a743081289dbc7b7b24ae55493a630
Endi Jean <endijean91@gmail.com>
2022-05-11 21:12:17 -0300

    Retornando dto

--- src/main/java/com/endijean/apitestes/resource/UserResource.java
+++ src/main/java/com/endijean/apitestes/resource/UserResource.java
@@ -1,7 +1,9 @@
 package com.endijean.apitestes.resource;
 
 import com.endijean.apitestes.domain.User;
+import com.endijean.apitestes.domain.dto.UserDTO;
 import com.endijean.apitestes.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.*;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value="/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
